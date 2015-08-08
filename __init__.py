f26358cf783aaca5a3a2fac22a7e8c79494e9699
Yves Lange <kursion@gmail.com>
2015-08-08 17:54:51 +0200

    Removing py_console (useless for that module)

--- __init__.py
+++ __init__.py
@@ -2,14 +2,13 @@ __all__ = ["crawler"]
 
 if __name__ == '__main__':
     import crawler
-    import py_console.colors as console
 
     proxies = []
     for page in range(1, 5):
-        console.success("Getting page %i on nntime.com" % (page))
-        proxies = proxies+proxy_crawler.getProxies(page)
+        print("Getting page %i on nntime.com" % (page))
+        proxies = proxies+crawler.getProxies(page)
     print(proxies)
-    console.header("Retrieved %i proxies" % (len(proxies)))
+    print("Retrieved %i proxies" % (len(proxies)))
 else:
     import crawler
     getProxies = crawler.getProxies
